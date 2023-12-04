cfad04ccf1a2bd6ebe4847c24bd4c30eb0cabb6e
Mrbilal <bilal_itchir@Outlook.fr>
2023-12-04 17:00:36 +0100

    🐛 corriger(index.php) : renommer la variable $numbers en $String ✨ fonctionnalité(index.php) : ajouter une fonction Vcount pour compter le nombre de voyelles dans une chaîne de caractères et l'afficher

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
-$numbers = [5, 17, 54, 2, 48, 35];
+$String = 'Algorithmie';
 
-echo max($numbers)
+function Vcount($String) {
+    $count = 0;
+    for ($i = 0; $i < strlen($String); $i++) {
+        $String[$i] == 'e' || $String[$i] == 'a' || $String[$i] == 'i' || $String[$i] == 'o' || $String[$i] == 'u' ? $count++ : null;
+    }
+    return $count;
+}
 
-?>
+echo Vcount($String);
